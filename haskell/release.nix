8493510fb216d297af358bfd989edaca9bd2ad06
Colin Woodbury <colingw@gmail.com>
2018-01-05 09:34:36 -0800

    Use Nix and update Haskell benchmarks
    - `countdownT` and `IO` ops have sped up

---
+++ haskell/release.nix
@@ -0,0 +1,17 @@
+let
+  config = {
+    packageOverrides = pkgs: {
+      haskellPackages = pkgs.haskellPackages.override {
+        overrides = haskellPackagesNew: haskellPackagesOld: {
+          scalaz-and-cats = haskellPackagesNew.callPackage ./default.nix { };
+        };
+      };
+    };
+  };
+
+  pkgs =
+    import <nixpkgs> { inherit config; };
+
+in
+  { scalaz-and-cats = pkgs.haskellPackages.scalaz-and-cats;
+  }
